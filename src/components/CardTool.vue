f7ecf7a577095ce158339e0fa94248f82de2e7be
cjlozano <carlosjlp@gmail.com>
2019-09-13 12:48:27 +0200

    estilos para el listado de herramientas y las card

--- src/components/CardTool.vue
+++ src/components/CardTool.vue
@@ -1,36 +1,44 @@
 <template>
-<div v-if="herramienta!== {}">
     <v-card
-      class="mx-auto"
-      :flat="flat"
-      :loading="loading"
-      :outlined="outlined"
-      :elevation="elevation"
-      :raised="raised">
-      <v-img
-        :src="herramienta.rutaIcon"
-        aspect-ratio="1"
-      >
-        <template v-slot:placeholder>
-          <v-layout
-            fill-height
-            align-center
-            justify-center
-            ma-0
-          >
-            <v-progress-circular indeterminate></v-progress-circular>
-          </v-layout>
-        </template>
-      </v-img>
-        <v-card-text>
-          <div class="text-card-principal">
-            <span class="text-card-principal__texto">
-              {{ herramienta.texto }}
+      :width="width"
+      :height="height"
+      outlined
+     >
+      <v-list-item three-line>
+        <v-list-item-content>
+          <v-list-item-title class="headline mb-1">{{ herramienta.titulo }}</v-list-item-title>
+          <v-list-item-subtitle class="card__subtitle">
+            <span>
+              {{ herramienta.subtitulo }}
             </span>
-          </div>
+          </v-list-item-subtitle>
+       </v-list-item-content>
+       <v-list-item-avatar
+          tile
+          size="80">
+          <v-img
+           :src="herramienta.rutaIcon"
+           width="20px"
+           aspect-ratio="1.5"
+         >
+           <template v-slot:placeholder>
+             <v-layout
+               fill-height
+               align-center
+               justify-center
+               ma-0
+             >
+              <v-progress-circular indeterminate></v-progress-circular>
+            </v-layout>
+           </template>
+          </v-img>
+        </v-list-item-avatar>
+      </v-list-item>
+      <v-card-text
+        class="card__text">
+        <span>{{ herramienta.texto }}</span>
       </v-card-text>
     </v-card>
-  </div>
 </template>
 <script>
 
@@ -54,6 +62,8 @@ export default {
       outlined: false,
       elevation: undefined,
       raised: false,
+      width: '250px',
+      height: '210px',
     };
   },
   methods: {
@@ -65,6 +75,15 @@ export default {
 </script>
 
 <style scoped>
+.card__subtitle {
+  font-weight: 700;
+}
+.card__text{
+  padding: 0 16px;
+}
+.card-tool {
+  display: flex;
+}
 .text-card-principal {
    text-align: center;
 }
--- src/components/ListTools.vue
+++ src/components/ListTools.vue
@@ -1,16 +1,15 @@
 <template>
-<v-layout row wrap justify-center>
-  <v-flex
-    xs6
-    sm3
-    md2
-    pa-2
-    v-for="(herramienta,index) in herramientasDisponibles"
-    :key="index">
-    <CardTool
-      :herramienta="herramienta"/>
-  </v-flex>
-</v-layout>
+<div class="listTools">
+  <div class="listTools__listado">
+    <div
+      class="listTools__listado__tool"
+      v-for="(herramienta,index) in herramientasDisponibles"
+      :key="index">
+      <CardTool
+        :herramienta="herramienta"/>
+    </div>
+  </div>
+</div>
 </template>
 <script>
 import CardTool from './CardTool.vue';
@@ -27,12 +26,16 @@ export default {
           {
             // eslint-disable-next-line global-require
             rutaIcon: require('../assets/icons/scissors.png'),
-            texto: 'Dividir Pdf',
+            titulo: 'Dividir pdf',
+            subtitulo: 'Extraer páginas de tu pdf',
+            texto: 'Extrae una o varias páginas de tu pdf.',
           },
           {
             // eslint-disable-next-line global-require
             rutaIcon: require('../assets/icons/join.png'),
-            texto: 'Unir Pdf',
+            titulo: 'Unir pdf',
+            subtitulo: 'Unir varios Pdfs',
+            texto: 'Une Pdfs en el orden que tu prefieras.',
           },
         ],
     };
@@ -41,4 +44,13 @@ export default {
 </script>
 
 <style scoped>
+.listTools__listado {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+.listTools__listado__tool {
+  padding: 8px;
+}
+
 </style>
