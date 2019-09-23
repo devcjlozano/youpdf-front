555f9a9734f1352ae3fe7d1317682f7d79d37396
cjlozano <carlosjlp@gmail.com>
2019-09-23 17:59:02 +0200

    Componente separado SnackBar y rangos repetidos

--- src/components/RangosPdf.vue
+++ src/components/RangosPdf.vue
@@ -1,103 +1,93 @@
 <template>
-<div class="rangosPdf">
-  <h2> Seleccionar páginas a extraer</h2>
-  <span
-    class="rangosPdf__info-paginas">
-    Número de páginas del pdf: <strong> {{ numeroPaginas }} </strong>
-  </span>
-  <div class="rangosPdf__contenedorInputs">
-    <v-snackbar
-      v-model="snackBar"
-      multi-line
-      color="error"
-      top
-      bottom
-      >
-        {{ textoErrorRango }}
-        <v-btn
-          text
-          @click="snackBar = false"
-        >
-        Cerrar
-      </v-btn>
-
-    </v-snackbar>
-    <div class="rangosPdf__contenedorInputs__inputs">
-      <v-text-field
-        :max="numeroPaginas"
-        :min="1"
-        v-model="desdePagina"
-        type="number"
-        label="De la página"
-        placeholder="Número de página"
-        outlined
-      ></v-text-field>
-      <v-text-field
-        :min="1"
-        :max="numeroPaginas"
-        v-model="hastaPagina"
-        class="rangosPdf__contenedorInputs_inputHasta"
-        type="number"
-        label="a la página"
-        placeholder="Número de página"
-        outlined
-      ></v-text-field>
-    </div>
-    <div class="rangosPdf__contenedor__buttons">
-      <div>
-        <v-btn
-          @click="anadirRango"
-          solid
-          color="primary">
-          Añadir rango
-        </v-btn>
+  <div class="rangosPdf">
+    <h2> Seleccionar páginas a extraer</h2>
+    <span
+      class="rangosPdf__info-paginas">
+      Número de páginas del pdf: <strong> {{ numeroPaginas }} </strong>
+    </span>
+    <div class="rangosPdf__contenedorInputs">
+      <div class="rangosPdf__contenedorInputs__inputs">
+        <v-text-field
+          :max="numeroPaginas"
+          :min="1"
+          v-model="desdePagina"
+          type="number"
+          label="De la página"
+          placeholder="Número de página"
+          outlined
+        ></v-text-field>
+        <v-text-field
+          :min="1"
+          :max="numeroPaginas"
+          v-model="hastaPagina"
+          class="rangosPdf__contenedorInputs_inputHasta"
+          type="number"
+          label="a la página"
+          placeholder="Número de página"
+          outlined
+        ></v-text-field>
       </div>
-      <div class="boton-enviar">
-        <v-btn
-          color="primary"
-          :disabled="rangos.length < 1"
-          solid>
-            <v-icon
-              left
-              class="icon-tijeras"> mdi-content-cut </v-icon>Dividir PDF
+      <div class="rangosPdf__contenedor__buttons">
+        <div>
+          <v-btn
+            @click="anadirRango"
+            solid
+            color="primary">
+            Añadir rango
           </v-btn>
+        </div>
+        <div class="boton-enviar">
+          <v-btn
+            color="primary"
+            :disabled="rangos.length < 1"
+            solid>
+              <v-icon
+                left
+                class="icon-tijeras"> mdi-content-cut </v-icon>Dividir PDF
+            </v-btn>
+        </div>
       </div>
     </div>
-  </div>
-  <div class="rangosPdf__contenedorTable">
-    <div style="margin-bottom:10px">
-      <span class="rangosPdf__info-rangos">  Rangos Seleccionados</span>
-    </div>
-    <div>
+    <div class="rangosPdf__contenedorTable">
+      <div style="margin-bottom:10px">
+        <span class="rangosPdf__info-rangos">  Rangos Seleccionados</span>
+      </div>
       <div>
-        <v-data-table
-          :headers="headers"
-          :items="rangos"
-          :page.sync="page"
-          :items-per-page="itemsPerPage"
-          no-data-text="No hay rangos seleccionados"
-          hide-default-footer
-          @page-count="pageCount = $event"
-          dense>
-          <template v-slot:item.action="{ item }">
-            <v-icon
-        small
-      >
-        mdi-delete-outline
-      </v-icon>
-          </template>
-        </v-data-table>
+        <div>
+          <v-data-table
+            :headers="headers"
+            :items="rangos"
+            :page.sync="page"
+            :items-per-page="itemsPerPage"
+            no-data-text="No hay rangos seleccionados"
+            hide-default-footer
+            @page-count="pageCount = $event"
+            dense>
+            <template v-slot:item.action="{ item }">
+              <v-icon
+          small
+        >
+          mdi-delete-outline
+        </v-icon>
+            </template>
+          </v-data-table>
+        </div>
+        <div class="rangosPdf__contenedorTable__footer">
+          <v-pagination
+            v-model="page"
+            :length="pageCount"></v-pagination>
+        </div>
       </div>
-      <div class="rangosPdf__contenedorTable__footer">
-        <v-pagination
-          v-model="page"
-          :length="pageCount"></v-pagination>
     </div>
-    </div>
+    <SnackBar
+      :color="colorSnackBar"
+      :snack-bar="snackBarVisible"
+      :mensaje="textoMensajeRango"
+      @cerrar-snackBar="cerrarSnackBar"/>
   </div>
-</div>
 </template>
 <script>
+import SnackBar from './SnackBar.vue';
 
 export default {
   name: 'RangosPdf',
@@ -112,16 +102,18 @@ export default {
     },
   },
   components: {
+    SnackBar,
   },
   data() {
     return {
       page: 1,
       pageCount: 0,
+      colorSnackBar: '',
       itemsPerPage: 6,
       desdePagina: 1,
       hastaPagina: 1,
-      snackBar: false,
-      textoErrorRango: '',
+      snackBarVisible: false,
+      textoMensajeRango: '',
       headers: [
         {
           text: 'Desde página',
@@ -150,9 +142,11 @@ export default {
       }
       const esValido = this.validarRango(desde, hasta);
       if (esValido.error) {
-        this.textoErrorRango = esValido.descripcionError;
-        this.snackBar = true;
+        this.abrirSnackBar(esValido.descripcionError, 'primary');
       } else {
+        if (esValido.warning) {
+          this.abrirSnackBar(esValido.descripcionWarning, 'warningCustom');
+        }
         const objectRango = {
           desde,
           hasta,
@@ -160,7 +154,7 @@ export default {
         this.rangos.push(objectRango);
       }
     },
-    validarRango(hasta, desde) {
+    validarRango(desde, hasta) {
       if (hasta < 1 || desde < 1) {
         return {
           error: true,
@@ -173,10 +167,26 @@ export default {
           descripcionError: 'No se puede seleccionar un rango que sobrepase el número de páginas del pdf',
         };
       }
+      if (this.rangos.findIndex(element => element.desde === desde
+        && element.hasta === hasta) !== -1) {
+        return {
+          warning: true,
+          descripcionWarning: `Se añade el rango seleccionado pero ten en cuenta que ya existe uno o 
+                varios rangos iguales`,
+        };
+      }
       return {
         error: false,
         descripcionError: '',
       };
+    },
+    abrirSnackBar(mensaje, color) {
+      this.textoMensajeRango = mensaje;
+      this.colorSnackBar = color;
+      this.snackBarVisible = true;
+    },
+    cerrarSnackBar() {
+      this.snackBarVisible = false;
     },
   },
 };
---
+++ src/components/SnackBar.vue
@@ -0,0 +1,51 @@
+<template>
+    <v-snackbar
+      v-model="snackBar"
+      multi-line
+      :color="color"
+      top
+      bottom
+      >
+        {{ mensaje }}
+        <v-btn
+          text
+          @click="cerrarSnackBar"
+        >
+        Cerrar
+      </v-btn>
+    </v-snackbar>
+</template>
+<script>
+
+export default {
+  name: 'SnackBar',
+  props: {
+    snackBar: {
+      type: Boolean,
+      default: false,
+    },
+    color: {
+      type: String,
+      default: '',
+    },
+    mensaje: {
+      type: String,
+      default: '',
+    },
+  },
+  components: {
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    cerrarSnackBar() {
+      this.$emit('cerrar-snackBar');
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
